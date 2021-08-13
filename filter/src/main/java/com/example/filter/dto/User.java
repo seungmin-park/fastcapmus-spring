94ce92e178b6c15460cf36bd2de7d7edf8f8ad30
seungmin-park <tmddudals369@naver.com>
2021-08-13 23:40:21 +0900

    filter interceptor

---
+++ filter/src/main/java/com/example/filter/dto/User.java
@@ -0,0 +1,15 @@
+package com.example.filter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+}
